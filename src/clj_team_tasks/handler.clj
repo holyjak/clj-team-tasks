422aefc56f1b7bc160f2c14425ffaef8e10b0fd6
Jakub Holy <jakub.holy@iterate.no>
2014-03-20 00:16:37 +0100

    wii register teams

--- src/clj_team_tasks/handler.clj
+++ src/clj_team_tasks/handler.clj
@@ -2,15 +2,27 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [net.cgrand.enlive-html :as html]))
+            [net.cgrand.enlive-html :as html]
+            [ring.util.response :refer [redirect]]))
 
+(def state (atom {
+                  :teams {}}))
+
+(defn rand-id [id-set]
+  (let [next-ids (repeatedly #(rand-int Integer/MAX_VALUE))]
+    (first (remove id-set next-ids))))
+
+
+(defn make-team [teams]
+  (let [id (rand-id (set (keys teams)))]
+    (assoc teams id {})))
 
 (html/deftemplate index-page "templates/index.html" [])
 ;;------------------------------------------------------------------------------------------------
 
 (defroutes app-routes
   (GET "/" [] (reduce str (index-page)))
-  (GET "/register" [] "Found")
+  (GET "/register" [] (str (swap! state #(update-in % [:teams] make-team)))) ;; TODO How to get the added ID so that we can redirect to it?!
   (route/resources "/")
   (route/not-found "Not Found"))
 
